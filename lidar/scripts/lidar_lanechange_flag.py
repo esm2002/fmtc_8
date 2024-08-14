73e248d5c8b902104c443b035332c5d980c56315
esm2002 <128345078+esm2002@users.noreply.github.com>
2024-08-14 14:31:24 +0900

    change lidar_lanechange_flag_cl.py to flag.py
    delete codes for parking

--- lidar/scripts/lidar_lanechange_flag.py
+++ lidar/scripts/lidar_lanechange_flag.py
@@ -33,23 +33,27 @@ class ObstacleDetection:
         self.count = 0 # Number of consecutive frames in which an obstacle is detected
         self.obstacle_flag = 0 # Flag indicating whether an obstacle is detected
         self.y_coord = 0 # y-coordinate of the obstacle
+        
 
     def cluster_coord_callback(self, msg):
         # Process the cluster coordinates
         self.cluster_coordinates = [(point.x, point.y) for point in msg.points]
         self.check_obstacle()
 
+
     def cluster_point_nums_callback(self, msg):
         # Process the cluster point numbers
         self.cluster_point_nums = msg.data
         self.check_obstacle()
 
+
     def check_obstacle(self):
         # Check if the specified conditions for obstacle detection are met
         
         # Check if the number of cluster coordinates and cluster point numbers are equal
+        # use 1 <= np.sqrt(x**2+y**2) <= 1.9?
         if len(self.cluster_coordinates) == len(self.cluster_point_nums):
-            obstacle_detected = any((1 <= np.sqrt(x**2+y**2) <= 1.9 and x<-1 and -1<y<1 and  num >= 30 and num<=70) for (x, y), num in zip(self.cluster_coordinates, self.cluster_point_nums))
+            obstacle_detected = any((-1<x<0 and 0<y<1 and  num >= 30 and num<=70) for (x, y), num in zip(self.cluster_coordinates, self.cluster_point_nums))
         else:
             obstacle_detected = False
 
@@ -58,17 +62,19 @@ class ObstacleDetection:
             self.count += 1
             # If the count exceeds 5, set the obstacle flag (5번 연속으로 장애물이 감지되면 장애물이 감지된 것으로 판단)
             # 5는 변경 가능, 0으로 두면 인식되자 마자 flag가 1로 바뀜
-            if self.count > 1: 
+            if self.count > 5: 
                 self.obstacle_flag = 1
                 
                 # Extract indices of elements that satisfy the obstacle_detected condition
-                indices = [index for index, value in enumerate(self.cluster_coordinates) if (1 <= np.sqrt(value[0]**2+value[1]**2) <= 2.1  and value[0]<-1 and -1<value[1]<1 and  self.cluster_point_nums[index] >= 10)]
+                # use 1 <= np.sqrt(value[0]**2+value[1]**2) <= 2.1?
+                indices = [index for index, value in enumerate(self.cluster_coordinates) if (-1<value[0]<0 and 0<value[1]<1 and  self.cluster_point_nums[index] >= 10)]
 
                 # If there are indices that satisfy the condition, find the index with the maximum y-coordinate
                 if indices:
-                    max_y_index = max(indices, key=lambda index: self.cluster_coordinates[index][1])
+                    min_y_index = min(indices, key=lambda index: self.cluster_coordinates[index][1])
+                    #change max to min
                     #self.y_coord = np.arctan(self.cluster_coordinates[max_y_index][1]/self.cluster_coordinates[max_y_index][0])*180/np.pi
-                    self.y_coord = self.cluster_coordinates[max_y_index][1]
+                    self.y_coord = self.cluster_coordinates[min_y_index][1]
                     print(self.y_coord)
 
         # If the conditions are not met, reset the count and obstacle flag            
@@ -78,11 +84,14 @@ class ObstacleDetection:
             self.obstacle_flag = 0
             self.y_coord = 0
 
+    
+
     def publish_obstacle_flag(self):
         # Publish the obstacle flag
         obstacle_flag_msg = obstacle_detection()
         obstacle_flag_msg.flag = self.obstacle_flag
         obstacle_flag_msg.y_coord = self.y_coord
+        
         self.obstacle_flag_pub.publish(obstacle_flag_msg)
 
 def main():
